572c07f0d7b68380725eeba53f61130f916e1703
Jonathan Sun <bakafish@berkeley.edu>
2020-03-10 16:28:06 -0700

    Add comments to PlayerTeleport.cs

--- Assets/Scripts/PlayerTeleport.cs
+++ Assets/Scripts/PlayerTeleport.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// Handles the process and visual effects of teleportation
 public class PlayerTeleport : MonoBehaviour
 {
+    // Player's spawn point
     public GameObject spawnPoint;
+    // Object containing the VR camera
     public GameObject cameraHolder;
+    // How fast the player sinks in water or whatever else is in a pit
     public float sinkSpeed;
+    // The player's rotation and position upon spawning
     private Quaternion spawnRotation;
     private Vector3 spawnPosition;
+    // Player respawn timer
     private float respawnTime;
+    // Controls the fade-to-color effect for VR camera
     private SteamVR_Fade fade;
+    // Rigidbody of the player, to which sinking forces are applied
     private Rigidbody playerRigidbody;
 
+    // Get spawn transform data, player rigidbody, and fade effect
     void Start()
     {
         spawnPosition = spawnPoint.transform.position;
@@ -23,6 +32,7 @@ public class PlayerTeleport : MonoBehaviour
         playerRigidbody = gameObject.GetComponent<Rigidbody>();
     }
 
+    // Start the respawn sequence if player hits water
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Water"))
@@ -33,28 +43,34 @@ public class PlayerTeleport : MonoBehaviour
         }
     }
 
+    // Moves the player back to the spawn point instantly
     void Respawn()
     {
         gameObject.transform.SetPositionAndRotation(spawnPosition, spawnRotation);
         fade.OnStartFade(Color.clear, 1.0f, false);
     }
 
+    // Control fade-to-color effect based on current respawn timer
     void Update()
     {
         if (respawnTime > 0.0f)
         {
             float lastRespawnTime = respawnTime;
             respawnTime -= Time.deltaTime;
+            // Start fade effect 1.5s before respawn
             if (lastRespawnTime >= 1.5f && respawnTime < 1.5f)
             {
                 fade.OnStartFade(new Color(0.0f, 0.03f, 0.1f), 1.0f, false);
             }
+            // If timer expires...
             if (respawnTime <= 0.0f)
             {
+                // ...respawn the player
                 respawnTime = -1.0f;
                 Respawn();
             } else
             {
+                // ...otherwise, sink the player more
                 playerRigidbody.velocity = new Vector3(playerRigidbody.velocity.x, -sinkSpeed, playerRigidbody.velocity.z);
             }
         }
