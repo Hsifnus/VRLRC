9a48953512284dfb2d5ff87ddea3b21eac59e42d
Jonathan Sun <bakafish@berkeley.edu>
2020-03-10 14:36:21 -0700

    Add comments to PlatformMovement.cs

--- Assets/Scripts/PlatformMovement.cs
+++ Assets/Scripts/PlatformMovement.cs
@@ -2,24 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Houses the logic behind platform movement
 public class PlatformMovement : MonoBehaviour
 {
+    // Location of pos1
     Vector3 position1;
+    // Location of pos2
     Vector3 position2;
+    // GameObject used as start point
     public GameObject pos1;
+    // GameObject used as end point
     public GameObject pos2;
+    // How fast the platform moves between points
     public float speed;
+    // Platform's rigidbody
     Rigidbody platform;
+    // The destination point of the platform
     Vector3 goalpoint;
+    // The platform's previous displacement from destination
     Vector3 prevdisp;
+    // The platform's current displacement from destination
     Vector3 curdisp;
+    // Full length of platform pause period
     public float maxpause;
+    // Timer that is initialized to maxpause every time platform starts pausing
     private float pause;
+    // Should this platform pause and turn around in response to hitting walls?
     public bool collisionResponse;
+    // Should this platform pause and turn around in response to hitting throwable objects?
     public bool throwableResponse;
+    // Whether this platform has collided with an obstruction en route to its destination
     private bool collided;
 
-    // Start is called before the first frame update
+    // Initialize platform parameters
     void Start()
     {
         position1 = pos1.transform.position;
@@ -32,7 +47,7 @@ public class PlatformMovement : MonoBehaviour
         collided = false;
     }
 
-    // Update is called once per frame
+    // Run platform pause timer, determine when it is time to pause, and update displacements
     void Update()
     {
         if (pause > 0)
@@ -42,7 +57,7 @@ public class PlatformMovement : MonoBehaviour
             return;
         }
         curdisp = transform.position - goalpoint;
-        if (Vector3.Dot(curdisp, prevdisp) <0 || collided)
+        if (Vector3.Dot(curdisp, prevdisp) < 0 || collided)
         {
             pause = maxpause;
             platform.velocity = new Vector3();
@@ -56,6 +71,7 @@ public class PlatformMovement : MonoBehaviour
         platform.velocity = -speed * curdisp.normalized;
     }
 
+    // Set the collided flag accordingly upon collision
     private void OnCollisionEnter(Collision collision)
     {
         GameObject thing = collision.gameObject;
