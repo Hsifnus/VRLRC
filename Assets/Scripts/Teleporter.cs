9efeecf2608e2a65e138af3a677c34bffee6063c
Jonathan Sun <bakafish@Js-MacBook-Pro.local>
2020-07-02 12:28:29 -0700

    Merge changes from temp branch into Teleporter script.

--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -2,58 +2,74 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Handles the teleportation of objects through the Teleporter
 public class Teleporter : MonoBehaviour
 {
-    //teleporter object
+    //Teleporter to teleport to
     public GameObject linkedTeleporter;
-
-    public float cooldown;
-
+    //Current puzzle state of teleporter
     private string state;
+    //Initial puzzle state of teleporter
     public string initialState = "on";
+    //How long the warp animation takes
     public float warpTime = 0.5f;
+    //VR fade effect manager
     public SteamVR_Fade fade;
+    //Location of linked Teleporter
+    Vector3 linkedPosition;
+    //Teleporter script attatched to the linked Teleporter
+    Teleporter linkedScript;
+    //Teleportable objects which should not be teleported
+    HashSet<string> teleportBlacklist;
 
+    //Get linked Teleporter data and create blacklist
     void Start()
     {
-        state = initialState;
+      state = initialState;
+      linkedPosition = linkedTeleporter.transform.position;
+      linkedScript = linkedTeleporter.GetComponent<Teleporter>();
+      teleportBlacklist = new HashSet<string>();
     }
 
-    private void WarpPlayer(GameObject player)
-    {
-        player.transform.SetPositionAndRotation(linkedTeleporter.transform.position, player.transform.rotation);
-        fade.OnStartFade(Color.clear, 0.5f, false);
-    }
-
-    // Carry player to linked teleport and allow them to leave on a cooldown
+    //Teleports the object entering if teleportable and not blacklisted
     private void OnTriggerEnter(Collider collision)
     {
-      GameObject player = collision.gameObject;
-      if (player.CompareTag("Player") && state != "off")
+      GameObject obj = collision.gameObject;
+      if (IsTeleportable(obj) && !teleportBlacklist.Contains(obj.name) && state != "off")
       {
-        linkedTeleporter.GetComponent<Collider>().enabled = false;
-        StartCoroutine(HandlePlayerTeleport(player));
+        //Adds object to linked teleporter blacklist so object does not instantly teleport back
+        linkedScript.teleportBlacklist.Add(obj.name);
+        //Play fade effect if player is being teleported
+        if (obj.CompareTag("Player")) {
+          StartCoroutine(HandlePlayerTeleport(obj));
+        }
+        obj.transform.SetPositionAndRotation(linkedPosition, obj.transform.rotation);
+      }
+    }
+
+    //Removes teleportable objects leaving Teleporter from the blacklist
+    private void OnTriggerExit(Collider collision)
+    {
+      GameObject obj = collision.gameObject;
+      if (IsTeleportable(obj) && teleportBlacklist.Contains(obj.name))
+      {
+        teleportBlacklist.Remove(obj.name);
       }
     }
 
     IEnumerator HandlePlayerTeleport(GameObject player)
     {
-        fade.OnStartFade(new Color(1f, 1f, 1f), warpTime, false);
-        yield return new WaitForSeconds(warpTime);
-        WarpPlayer(player);
-        yield return new WaitForSeconds(cooldown);
-        EnableTeleport();
+      //Start fade to white
+      fade.OnStartFade(new Color(1f, 1f, 1f), warpTime, false);
+      yield return new WaitForSeconds(warpTime);
+      //Clear fade and warp the player once warp time has elapsed
+      player.transform.SetPositionAndRotation(linkedTeleporter.transform.position, player.transform.rotation);
+      fade.OnStartFade(Color.clear, 0.5f, false);
     }
 
-    // Enables teleported back from the linked teleporter
-    private void EnableTeleport()
+    //Determines whether an object is of a teleportable type
+    private bool IsTeleportable(GameObject obj)
     {
-      linkedTeleporter.GetComponent<Collider>().enabled = true;
-    }
-
-    // Sets teleporter state
-    public void SetState(string newState)
-    {
-        state = newState;
+      return obj.CompareTag("Player") || obj.CompareTag("Throwable");
     }
 }
